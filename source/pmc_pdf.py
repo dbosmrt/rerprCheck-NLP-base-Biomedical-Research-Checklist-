1b06d0071c2af410b96c8c5a5e741b23c07761ef
dbosmrt <deepanshu.2006bhatt@gmail.com>
2025-08-17 10:19:04 +0530

    Update pmc_pdf.py

--- source/pmc_pdf.py
+++ source/pmc_pdf.py
@@ -1,27 +1,38 @@
 import os
+import pandas as pd
 import requests
 
-# Base URL
-base_url = "https://www.ncbi.nlm.nih.gov/pmc/articles/"
+def download_pdf(pmc_id, save_dir):
+    os.makedirs(save_dir, exist_ok = True)
 
-def download_pdf(pmc_id, output_dir = "pdfs"):
-    os.makedirs(output_dir, exist_ok=True)
-
-    url = f"{base_url}{pmc_id}/pdf/"
+    url = f"https://www.ncbi.nlm.nih.gov/pmc/articles/{pmc_id}/pdf/"
 
     try:
-        response = requests.get(url, timeout=20)
+        response = requests.get(url, stream = True , timeout = 20)
 
-        if response.status_code==200 and response.headers.get("Content-Type")== "application/pdf":
-            filepath = os.path.join(output_dir, f"{pmc_id}.pdf")
+        if response.status_code == 200 and "application/pdf" in response.headers.get("Content-Type", ""):
+            filepath = os.path.join(save_dir, f"{pmc_id}.pdf")
             with open(filepath, "wb") as f:
-                f.write(response.content)
+                for chunk in response.iter_content(chunk_size=1024):
+                    if chunk:
+                        f.write(chunk)
+
             print(f"[+] PDF saved: {filepath}")
             return filepath
         else:
-            print(f"[-] No PDF available for {pmc_id}")
+            print(f"[-] No PDF available for {pmc_id} (Status: {response.status_code})")
             return None
         
     except Exception as e:
-        print(f"[!] Error downloading PDF for {pmc_id} : {e}")
+        print(f"[!] Error downloading PDF for {pmc_id}: {e}")
         return None 
+    
+
+if __name__ == "__main__":
+    pmc_csv = r"C:\Users\deepa\Downloads\Deepanshu Bhatt\rerprCheck-NLP-base-Biomedical-Research-Checklist-\Data\raw\pmc_ids.csv"
+    pmc_df = pd.read_csv(pmc_csv)
+
+    pdf_output_dir = r"C:\Users\deepa\Downloads\Deepanshu Bhatt\rerprCheck-NLP-base-Biomedical-Research-Checklist-\Data\pdfs"
+
+    for pmc_id in pmc_df["PMC_ID"]:
+        download_pdf(pmc_id, pdf_output_dir)
