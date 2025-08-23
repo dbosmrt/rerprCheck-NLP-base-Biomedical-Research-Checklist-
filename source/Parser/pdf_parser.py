25b476746f56db0a5f1102b72958145d9038e29f
dbosmrt <deepanshu.2006bhatt@gmail.com>
2025-08-23 21:26:46 +0530

    Update pdf_parser.py

--- source/Parser/pdf_parser.py
+++ source/Parser/pdf_parser.py
@@ -1,4 +1,20 @@
 from PyPDF2 import PdfReader
+import os
+
+folder_path = "C:\Users\deepa\Downloads\Deepanshu Bhatt\rerprCheck-NLP-base-Biomedical-Research-Checklist-\Data\raw\pmc_pdfs"
 
 # We will make all a loop for all of the pdfs:
-for 
+
+for file in os.listdir(folder_path):
+    if file.endswith(".pdf"):
+        file_path = os.path.join(file, folder_path)
+
+    # Iteriate over all the files and collect the metadata
+    try:
+        reader = PdfReader(file_path)
+        num_pages = len(reader.pages)
+        meta = len(reader.metadata)
+        print(f"Processing : {file}")
+        print(f"Processing : (metadata)")
+        
+        
