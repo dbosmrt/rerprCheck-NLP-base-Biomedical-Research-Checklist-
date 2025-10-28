35a4957d21bc441e8aac055b4a8c23211c48dc3a
dbosmrt <deepanshu.2006bhatt@gmail.com>
2025-10-28 17:05:18 +0530

    Update weak_supervision.py

--- source/NLP/weak_supervision.py
+++ source/NLP/weak_supervision.py
@@ -8,3 +8,17 @@ This file will contain all the classes and methods for the weak supervision of t
 Skweak Library, using spacy's {"en_core_sci_scibert model"} for ner.
 And storing of the labelled or Training dataset in the prescribed folder path.
 """
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+logger = logging.getLogger(__name__)
+
+
+class WeakSupervision:
+    """
+    This class conatains methods for weak supervison of the text file.
+    """
+    def 
